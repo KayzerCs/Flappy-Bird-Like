7e43bceb95019e118396ae9a6a8a7b8caa1dc7f8
KayzerPcPortable <max.mlv2210@gmail.com>
2024-06-01 17:49:22 +0200

    load image

--- script.js
+++ script.js
@@ -9,17 +9,31 @@ let birdWidth = 34; // width/height ratio = 408/228 = 17/12
 let birdHeight = 24;
 let birdX = boardWidth / 8;
 let birdY = boardHeight / 2;
+let birdImg;
 
 let bird = {
-    x : birdX,
-    y : birdY,
-    width : birdWidth,
-    height : birdHeight
-}
+  x: birdX,
+  y: birdY,
+  width: birdWidth,
+  height: birdHeight,
+};
 
 window.onload = function () {
   board = document.getElementById("board");
   board.height = boardHeight;
   board.Width = boardWidth;
   context = board.getContext("2d"); // utilisé pour dessiner au tableau
+
+  // dessiner flappy bird
+//   context.fillStyle = "green";
+//   context.fillRect(bird.x, bird.y, bird.width, bird.height);
+
+  // charger l'image
+  birdImg = new Image();
+  birdImg.src = "./asset/flappybird.png";
+  birdImg.onload = function () {
+    context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
+  };
+
 };
+
