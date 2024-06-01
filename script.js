b23d56d4378ad29134035b8736c607307ed0637e
KayzerPcPortable <max.mlv2210@gmail.com>
2024-06-01 17:07:50 +0200

    bird

--- script.js
+++ script.js
@@ -4,6 +4,19 @@ let boardWidth = 360;
 let boardHeight = 640;
 let context;
 
+//bird
+let birdWidth = 34; // width/height ratio = 408/228 = 17/12
+let birdHeight = 24;
+let birdX = boardWidth / 8;
+let birdY = boardHeight / 2;
+
+let bird = {
+    x : birdX,
+    y : birdY,
+    width : birdWidth,
+    height : birdHeight
+}
+
 window.onload = function () {
   board = document.getElementById("board");
   board.height = boardHeight;
